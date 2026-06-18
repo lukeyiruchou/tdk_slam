fa3d5ef92b0e52ed7609dfbca94247ad85c1ef56
terryking0711 <terry9487turry0711@gmail.com>
2026-06-18 07:10:59 +0000

    modify start_slam,sh

--- start_slam.sh
+++ start_slam.sh
@@ -8,13 +8,19 @@ tmux new-session -d -s $SESSION
 # Pane 0: maze_world
 tmux send-keys -t $SESSION:0 "$SOURCE && ros2 launch tdk_slam_manager maze_world_launch.py" Enter
 
+sleep 1
+
 # Pane 1: sim_spawn
 tmux split-window -h -t $SESSION:0
 tmux send-keys -t $SESSION:0.1 "$SOURCE && ros2 launch tdk_slam_manager sim_spawn_launch.py" Enter
 
+sleep 1
+
 # Window 1 Pane 0: nav2
 tmux new-window -t $SESSION:1
 tmux send-keys -t $SESSION:1 "$SOURCE && ros2 launch tdk_nav2_manager nav_launch.py" Enter
+
+sleep 10
 
 # Window 1 Pane 1: rviz2
 tmux split-window -h -t $SESSION:1
