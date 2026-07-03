58b5e2de74bfc4eec5b1aa5bdf9dd4b4b6e81120
terryking0711 <terry9487turry0711@gmail.com>
2026-07-03 10:40:53 +0800

    modify filter angle of lidars

--- src/tdk_slam_manager/launch/spawn_launch.py
+++ src/tdk_slam_manager/launch/spawn_launch.py
@@ -74,8 +74,8 @@ def generate_launch_description():
         executable='laser_angle_filter_node',
         name='filter_front',
         parameters=[{
-            'lower_angle': -1.5708,
-            'upper_angle': -3.1415,
+            'lower_angle': -2.7489,
+            'upper_angle': -1.9635,
             'input_topic': '/front/scan',
             'output_topic': '/front/scan_filtered'
         }]
@@ -86,8 +86,8 @@ def generate_launch_description():
         executable='laser_angle_filter_node',
         name='filter_rear',
         parameters=[{
-            'lower_angle': -1.5708,
-            'upper_angle': -3.1415,
+            'lower_angle': -2.7489,
+            'upper_angle': -1.9635,
             'input_topic': '/rear/scan',
             'output_topic': '/rear/scan_filtered'
         }]
